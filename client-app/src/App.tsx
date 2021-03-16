da695452c96cc94d35823980717cd97ad81be9e5
Jim Shaw <jimxshaw@gmail.com>
2021-03-15 22:01:12 -0500

    Use axios to display a list of events.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/events').then(response => {
+      console.log(response);
+      setEvents(response.data);
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {events.map((event: any) => (
+            <li key={event.id}>
+              {event.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
